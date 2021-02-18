7be91f2876f7e2165fe57d1f9b389e207fa8d644
akash chowrasia <akash@plaxonic.com>
2021-02-19 02:15:26 +0530

    added app

--- Back-end/temp.py
+++
@@ -1,8 +0,0 @@
-import csv
-with open('user-transaction.csv','a',newline='') as f:
-    thewriter = csv.writer(f)
-    temp = ['ID','TOTAL']
-    thewriter.writerow(temp)
-    for i in range(1,101):
-        temp = [i,0]
-        thewriter.writerow(temp)
